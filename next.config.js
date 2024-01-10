5bab840809c395cb16d4ca9ef4b1307d9a2caac5
ancalgia <ancalgia@gmail.com>
2024-01-11 05:14:06 +0900

    設定ファイルの更新（なんだっけこれ？）

--- next.config.js
+++ next.config.js
@@ -3,6 +3,31 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   experimental: { appDir: true },
+  async rewrites() {
+    return [
+      {
+        source: "/hogera",
+        destination: "https://api.battle.pokemon-home.com/tt/cbd/competition/rankmatch/list",
+      },
+    ];
+  },
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
