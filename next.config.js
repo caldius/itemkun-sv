bbaa02e11d0425ce2cb75199aea3442fc39fc6f2
ancalgia <ancalgia@gmail.com>
2024-01-17 04:37:13 +0900

    公開準備

--- next.config.js
+++ next.config.js
@@ -1,33 +1,35 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",
   reactStrictMode: true,
   swcMinify: true,
   experimental: { appDir: true },
-  async rewrites() {
-    return [
-      {
-        source: "/hogera",
-        destination: "https://api.battle.pokemon-home.com/tt/cbd/competition/rankmatch/list",
-      },
-    ];
-  },
-  async headers() {
-    return [
-      {
-        source: "/(.*)",
-        headers: [
-          { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "*" },
-          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
-          {
-            key: "Access-Control-Allow-Headers",
-            value:
-              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
-          },
-        ],
-      },
-    ];
-  },
+  // TODO:ここ静的出力したい場合どうしたらいいのか不明
+  // async rewrites() {
+  //   return [
+  //     {
+  //       source: "/hogera",
+  //       destination: "https://api.battle.pokemon-home.com/tt/cbd/competition/rankmatch/list",
+  //     },
+  //   ];
+  // },
+  // async headers() {
+  //   return [
+  //     {
+  //       source: "/(.*)",
+  //       headers: [
+  //         { key: "Access-Control-Allow-Credentials", value: "true" },
+  //         { key: "Access-Control-Allow-Origin", value: "*" },
+  //         { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+  //         {
+  //           key: "Access-Control-Allow-Headers",
+  //           value:
+  //             "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+  //         },
+  //       ],
+  //     },
+  //   ];
+  // },
 };
 
 module.exports = nextConfig;
